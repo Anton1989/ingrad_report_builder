4e59b55e0304f210fbb6d80a662658bc8076c7f1
Anton Abashin <wemakesites.nn@gmail.com>
2018-01-30 11:04:19 +0300

    in progress

--- src/frontend/modules/map/containers/Map.jsx
+++ src/frontend/modules/map/containers/Map.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import bindActionCreators from 'redux/lib/bindActionCreators';
 import connect from 'react-redux/lib/connect/connect';
 import browserHistory from 'react-router/lib/browserHistory';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, Polygon } from 'react-google-maps';
+import { compose, withStateHandlers } from 'recompose';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, Polygon, InfoWindow } from 'react-google-maps';
 //Actions
 import { getPlaces, dismissError } from '../actions/placesActions';
 //Components
@@ -27,6 +28,12 @@ const COLORS = {
 
 class Map extends React.Component {
 
+    constructor(...props) {
+        super(...props);
+
+        // this.handleMapLoaded = this.handleMapLoaded.bind(this);
+    }
+
     componentDidMount() {
         if (this.props.places.data.length == 0) this.props.getPlaces();
     }
@@ -46,48 +53,82 @@ class Map extends React.Component {
         return defaultCoordinates;
     }
 
+    // handlePolygonClick(event, place) {
+    //     var contentString = '<b>' + place.name + '</b><br>' +
+    //         'Clicked location: <br>' + event.latLng.lat() + ',' + event.latLng.lng() +
+    //         '<br>';
+    //     this.info.setContent(contentString);
+    //     this.info.setPosition(event.latLng);
+    //     console.log(this.info, this.gmap)
+    //     this.info.open(this.gmap);
+    // }
+
     render() {
         const { places, type, activeTypes, placeId, openPlace } = this.props;
         console.log('RENDER <Map>');
 
         let defaultCoordinates = this.getDefaultCoordinates(places.data, activeTypes, type, placeId);
 
-        let Maps = withScriptjs(withGoogleMap(() => <GoogleMap
-            defaultZoom={defaultCoordinates.zoom}
-            defaultCenter={defaultCoordinates.coordinates}
-        >
-            {places.data && places.data.map(place => {
-                if (activeTypes.indexOf(place.location) !== -1) {
-                    let placeHtml = null;
-                    if (placeId && place.houses.length > 0) {
-                        placeHtml = place.houses.map(house => {
-                            // console.log(house)
-                            return <Polygon
-                                key={place._id+house.name}
-                                paths={house.coordinates}
-                                options={{
-                                    strokeColor: '#' + COLORS[house.status],
-                                    strokeOpacity: 0.8,
-                                    strokeWeight: 2,
-                                    fillColor: '#' + COLORS[house.status],
-                                    fillOpacity: 0.35
-                                }}
-                            />
-                        });
-                    } else {
-                        placeHtml = <Marker
-                            key={place._id}
-                            defaultIcon={{ url: place.logo, scaledSize: new google.maps.Size(30, 30) }}
-                            position={{ ...place.coordinates }}
-                            onClick={() => { openPlace(place.location, place._id) }} />;
+        let Maps = compose(
+            withStateHandlers(
+                () => ({
+                    infoWindow: null,
+                }),
+                {
+                    onToggleOpen: ({ infoWindow }) => (event, house) => {
+                        console.log(event, house, infoWindow);
+                        return ({
+                            infoWindow: <InfoWindow
+                                position={{ lat: event.latLng.lat(), lng: event.latLng.lng() }}
+                            >
+                                <p>
+                                    <b>{house.name}</b><br/>
+                                    Статус <b>{house.status}</b><br/>
+                                </p>
+                            </InfoWindow>,
+                        })
                     }
-                    
-                    return placeHtml;
                 }
-            }
-            )}
-        </GoogleMap>));
-
+            ),
+            withScriptjs,
+            withGoogleMap
+        )(props => {
+            return <GoogleMap
+                defaultZoom={defaultCoordinates.zoom}
+                defaultCenter={defaultCoordinates.coordinates}
+            >
+                {places.data && places.data.map(place => {
+                    if (activeTypes.indexOf(place.location) !== -1) {
+                        let placeHtml = null;
+                        if (placeId && place.houses.length > 0 && placeId == place._id) {
+                            placeHtml = place.houses.map(house => {
+                                // console.log(house)
+                                return <Polygon
+                                    key={place._id + house.name}
+                                    paths={house.coordinates}
+                                    onClick={(event) => { props.onToggleOpen(event, house) }}
+                                    options={{
+                                        strokeColor: '#' + COLORS[house.status],
+                                        strokeOpacity: 0.8,
+                                        strokeWeight: 2,
+                                        fillColor: '#' + COLORS[house.status],
+                                        fillOpacity: 0.35
+                                    }}
+                                />
+                            });
+                        } else {
+                            placeHtml = <Marker
+                                key={place._id}
+                                defaultIcon={{ url: place.logo, scaledSize: new google.maps.Size(30, 30) }}
+                                position={{ ...place.coordinates }}
+                                onClick={() => { openPlace(place.location, place._id) }} />;
+                        }
+                        return placeHtml;
+                    }
+                })}
+                {props.infoWindow}
+            </GoogleMap>
+        });
         return <div>
             <Maps
                 googleMapURL='https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCK5pMR00_zxULM5AVzW5BNfiBpt6svVtk&signed_in=true'
